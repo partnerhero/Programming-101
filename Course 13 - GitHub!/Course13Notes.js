a41df9b082771835602f255861dae7f7cd04f418
Josh <josh.alex.wilson@gmail.com>
2020-08-05 16:09:55 +0200

    added some course notes

---
+++ Course 13 - GitHub!/Course13Notes.js
@@ -0,0 +1,52 @@
+// What is GitHub?
+// I don't know lol
+
+// git repository
+// is a virtual storage of your project. It allows you to save versions of your code, which you can access when needed. REWORD THIS
+// Executing this command will create a new .git subdirectory in your current working directory. This will also create a new master branch. REWORD THIS
+
+/* What is a branch?
+No clue fml
+local
+remote
+origin - 
+*/
+
+/* Useful Commands
+
+git init - creates a new repo (repository)
+
+git clone - copies an existing repository while creating a new repository. No need to use git init if a repository already exists. git clone goes through the initialization process for you.
+
+git status - shows you the status of any changes you've made
+
+git branch - shows the available brnahces and highlights to branch you're on
+
+git pull - updates your local directory with any new changes????
+
+git add - adds your changes to something?
+git add . - adds all changes
+
+git commit - adds your added changes to something else lol? local branch?
+git commit -m "Message here" - commits changes with a note about the changes
+
+git push - pushes commited changes to remote branch
+git push -u - maybe??? why???
+
+
+
+*/
+
+/*
+
+Creating a new repository and connecting it to github
+1. Navigate to a folder you want that you want to make into a github repo
+2. git init - to initialize the git process and create a brand new repo
+3. git add . - to add the files to a staging area WHAT GOOD IS THIS FOR???
+4. git commit -m "<notes>" - to commit the changes to SOMET?HING!!
+5. Log in to your github account and click on new repository and create repository
+6. You should see some instructions there, which we will follow
+7. git remote add origin https://github.com/username/new_repo - WHAT DOES THIS DO SPECIFICALLY?
+8. git push -u origin master - this pushes your local repo to your github account
+9. Check your github account and see that your work has been pushed to github
+*/
