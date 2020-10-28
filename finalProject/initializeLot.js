d3fc3b8872095c1f2019b40697515630aa3dec6e
Josh Wilson <58996473+joshua-alex@users.noreply.github.com>
2020-10-28 19:20:52 +0100

    add random number generator to initializeLot

--- finalProject/initializeLot.js
+++ finalProject/initializeLot.js
@@ -9,28 +9,28 @@ module.exports = () =>
         db = new JsonDB(new Config('carLot', true, true, '/'));
         if (exists === false) {
             db.push('/carArray[0]', {
-                id: 59742,
+                id: Math.floor(Math.random() * 99999),
                 make: 'Toyota',
                 model: 'Camry',
                 year: 2005,
                 price: 7000,
             });
             db.push('/carArray[1]', {
-                id: 65587,
+                id: Math.floor(Math.random() * 99999),
                 make: 'Ford',
                 model: 'Ranger',
                 year: 2002,
                 price: 6000,
             });
             db.push('/carArray[2]', {
-                id: 32471,
+                id: Math.floor(Math.random() * 99999),
                 make: 'Honda',
                 model: 'Civic',
                 year: 1999,
                 price: 3000,
             });
             db.push('/carArray[3]', {
-                id: 33968,
+                id: Math.floor(Math.random() * 99999),
                 make: 'Dodge',
                 model: 'Dart',
                 year: 2017,
