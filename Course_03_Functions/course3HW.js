875900be314ef11544681a3f06bd6921a211c893
Josh Wilson <josh.alex.wilson@gmail.com>
2020-12-10 22:05:34 +0100

    add homework prompts

---
+++ Course_03_Functions/course3HW.js
@@ -0,0 +1,26 @@
+/*
+Functions homework!
+
+https://repl.it/languages/nodejs
+
+1. Copy and paste the following function into https://repl.it/languages/nodejs. Then inside the console.log(), call the function and run it.
+
+const greeting = () => {
+    return 'Hey there! :)';
+};
+console.log();
+
+2. Write a function with no parameters that returns any string. Call the function inside a console.log() and then run it.
+
+3. Write a function with one parameter that multiplies that parameter by 2. Call the function inside a console.log() and pass in the number 5 as an argument then run it.
+
+4. Write a function that adds two parameters together. Call the function inside a console.log() and pass any two numbers as arguments. Call this function three times with three different sets of numbers.
+
+5. Write a function that converts meters to feet. 1 meter is 3.28 feet, so your function block should multiply meters by 3.28. When calling the function, it should take meters as an argument.
+
+6. Write a function with one parameter that returns true if it is greater than 50 and returns false if it is less than 50.
+
+BONUS:
+Write a function that checks two given numbers and returns true if one of the numbers is 50 or if the sum of both numbers is 50, otherwise return false.
+
+*/
