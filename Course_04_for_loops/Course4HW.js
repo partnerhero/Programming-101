f27a420edb33bbb27aeb8c66583722f1d0358c36
Josh Wilson <josh.alex.wilson@gmail.com>
2020-12-22 03:21:29 +0100

    added HW answers

--- Course_04_for_loops/Course4HW.js
+++ Course_04_for_loops/Course4HW.js
@@ -5,15 +5,41 @@
 // 1. Use the += operator to reassign and increase the following variable by 3 and then log it to the console.
 let myVariable = 5;
 
-// 2. Copy and paste the following for loop. Now add console.log(i); between the curly braces so that i is logged to the console with each iteration.
+// ANSWER:
+myVariable += 3;
+console.log(myVariable); // output: 8
 
+// 2. Copy and paste the following for loop. Now add console.log(i); between the curly braces so that i is logged to the console with each iteration.
 for (let i = 0; i <= 10; i++) {}
+
+// ANSWER:
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
 
 // 3. Create a for loop that iterates from 0 through 20 by 2, and then logs the iterator variable to the console for each iteration.
 
+// ANSWER:
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
 // 4. Create a for loop that iterates from 100 down to and including 0 by steps of 5. Log the iterator variable to the console with each iteration.
 
+// ANSWER:
+for (let i = 100; i >= 0; i -= 5) {
+    console.log(i);
+}
+
 // 5. Create a for loop that iterates from 1 through 20 by 1 and logs the variable each time. Inside the block of code, write a statement such that if the iterator variable is strictly equal to 10, the string 'You are halfway!' is logged to the console.
+
+// ANSWER:
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+    if (i === 10) {
+        console.log('You are halfway!');
+    }
+}
 
 // 6. Create a global variable named theCount and set it equal to 20. Create a while loop such that while theCount is greater than 0, theCount is decreased by 1 and then logged to the console.
 
@@ -39,7 +65,7 @@ while (theCount > 0) {
 // ANSWER:
 let hashtag = '#';
 
-for (let i = 0; i <= 7; i++) {
+for (let i = 0; i <= 5; i++) {
     console.log(hashtag);
     hashtag += '#';
 }
