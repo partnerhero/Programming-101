6d16fd203da660448772e698f3d7094f19f8f211
Josh Wilson <josh.alex.wilson@gmail.com>
2021-02-18 23:46:08 +0100

    course 12 cleanup and course 7 minor fix

--- Course_07_Arrays/Course7HWAnswers.js
+++ Course_07_Arrays/Course7HWAnswers.js
@@ -29,7 +29,9 @@ What will the output be in the console? Check to see if you're right!
 */
 
 // 4. a. What is the length of this array?: const bands = ['Radiohead', 'The Beatles', 'Red Hot Chili Peppers'];
+console.log(bands.length);
 // b. At what index of the array is 'Red Hot Chili Peppers' located?
+console.log(bands.indexOf('Red Hot Chili Peppers'));
 
 // 5. Using the original array in question 3 (const hobbies = ['playing video games', 'snorkeling', 'rock climbing', 'playing piano']), write a for loop that iterates through the array and prints "<hobby> is fun to do!" for each hobby in the array.
 
