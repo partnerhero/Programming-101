9b2ba737a623b0840a3bfbf467244ae368368d8f
Josh Wilson <josh.alex.wilson@gmail.com>
2021-06-15 22:05:20 +0200

    fix export statement

--- Course_20_Module_Exports/updateUserWebsite.js
+++ Course_20_Module_Exports/updateUserWebsite.js
@@ -3,7 +3,7 @@ const utilites = require('./utilities');
 const fetchUserData = utilites.fetchUserData;
 const updateUserData = utilites.updateUserData;
 
-const updateUserWebsite = (userId, website) =>
+module.exports = (userId, website) =>
     new Promise((resolve, reject) => {
         fetchUserData(userId).then(userData => {
             userData.website = website;
@@ -12,5 +12,3 @@ const updateUserWebsite = (userId, website) =>
             });
         });
     });
-
-module.exports = updateUserWebsite;
